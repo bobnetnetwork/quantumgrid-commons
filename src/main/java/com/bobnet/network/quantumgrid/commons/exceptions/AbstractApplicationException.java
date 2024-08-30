a6babad3b7a06f6b721abde511bd8494d512f829
Bobesz <bob@bobnet.network>
2024-08-30 12:18:26 +0200

    Create AbstractApplicationException.java

---
+++ src/main/java/com/bobnet/network/quantumgrid/commons/exceptions/AbstractApplicationException.java
@@ -0,0 +1,34 @@
+package com.bobnet.network.quantumgrid.commons.exceptions;
+
+/**
+ * The abstract base class for all application-specific exceptions.
+ */
+public abstract class AbstractApplicationException extends RuntimeException {
+
+    /**
+     * Constructs a new application exception with {@code null} as its detail message.
+     */
+    protected AbstractApplicationException() {
+        super();
+    }
+
+    /**
+     * Constructs a new application exception with the specified detail message.
+     *
+     * @param message the detail message
+     */
+    protected AbstractApplicationException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new application exception with the specified detail message
+     * and cause.
+     *
+     * @param message the detail message
+     * @param cause the cause
+     */
+    protected AbstractApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
